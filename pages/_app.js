9f40392aabdbeaf304be544e24e35777af576e11
doquang0310 <38870227+doquang0310@users.noreply.github.com>
2022-01-08 01:37:41 +0700

    fix
    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import '../styles/global-noscroll.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
